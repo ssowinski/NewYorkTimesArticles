d0cdd4fd098320725ab40c1acb654e7eac1a7818
Slawomir Sowinski <sl.sowinski@gmail.com>
2018-03-06 23:53:53 +0100

    Clean up DefaultNetworkService.

--- NewYorkTimesArticles/Networking/DefaultNetworkService.swift
+++ NewYorkTimesArticles/Networking/DefaultNetworkService.swift
@@ -10,33 +10,38 @@ import Foundation
 
 class DefaultNetworkService: NetworkService {
     
-    private let urlSession : URLSession
-    private let sessionConfig : URLSessionConfiguration
+    private let urlSession : URLSession = URLSession.shared
     private var dataTask: URLSessionDataTask?
-    
-    init() {
-        sessionConfig = URLSessionConfiguration.default
-        sessionConfig.timeoutIntervalForRequest = 20.0
-        sessionConfig.timeoutIntervalForResource = 30.0
-        urlSession = URLSession(configuration: sessionConfig)
-    }
     
     func startRequest(_ requestTarget: RequestTargetType, completionHandler: @escaping (HTTPURLResponse?, Data?, Error?) -> Void) {
         
         if dataTask != nil {
             dataTask?.cancel()
         }
-        
-        sessionConfig.httpAdditionalHeaders = requestTarget.headers
-        
-        let stringParameters = NetworkingTools.parametersToString(requestTarget.parameters)
-        
-        guard let url = URL(string: "\(requestTarget.path)\(stringParameters)") else {
+
+        let urlString: String
+        if requestTarget.parameterEncoding == .url {
+            let urlParameters = NetworkingTools.parametersToString(requestTarget.parameters)
+            urlString = "\(requestTarget.path)\(urlParameters)"
+        } else {
+            urlString = "\(requestTarget.path)"
+        }
+
+        guard let url = URL(string: urlString) else {
             //TODO: -completionHandler with error
             return
         }
         
-        dataTask = urlSession.dataTask(with: url) { data, response, error in
+        var request =  URLRequest(url: url)
+        request.httpMethod = requestTarget.method.rawValue
+        request.allHTTPHeaderFields = requestTarget.headers
+        
+        if requestTarget.parameterEncoding == .json, let jsonData = requestTarget.parameters?.data {
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = jsonData
+        }
+        
+        dataTask = urlSession.dataTask(with: request) { data, response, error in
             DispatchQueue.main.async {
                 completionHandler(response as? HTTPURLResponse, data, error)
             }
