96e56f977910593357dad87c243161fa1ddec9dc
Slawomir Sowinski <sl.sowinski@gmail.com>
2018-03-06 23:47:07 +0100

    Added fullDateTime in Date extension.

--- NewYorkTimesArticles/Shared/Extension/Date+String.swift
+++ NewYorkTimesArticles/Shared/Extension/Date+String.swift
@@ -14,7 +14,7 @@ extension Date {
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.locale = Locale(identifier: "en_US_POSIX")
         //        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX" //data musi byc z timezone, bez timezone system myśli ze to jest UTC i dodaje 2 bo my jesteśmy w Warszawaie
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
         return formatter
     }()
     
@@ -28,3 +28,16 @@ extension String {
         return Date.Iso8601Formatter.date(from: self)
     }
 }
+
+extension Date {
+    static let FullDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter
+    }()
+    
+    var fullDateTime: String {
+        return Date.FullDateFormatter.string(from: self)
+    }
+}
+
