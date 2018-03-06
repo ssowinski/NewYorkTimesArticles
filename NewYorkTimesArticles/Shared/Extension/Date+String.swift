48130d277b812e32539056efb5a1c8c8b535785f
Slawomir Sowinski <sl.sowinski@gmail.com>
2018-03-06 23:48:18 +0100

    Clean up Date+String.

--- NewYorkTimesArticles/Shared/Extension/Date+String.swift
+++ NewYorkTimesArticles/Shared/Extension/Date+String.swift
@@ -13,7 +13,6 @@ extension Date {
         let formatter = DateFormatter()
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.locale = Locale(identifier: "en_US_POSIX")
-        //        formatter.timeZone = TimeZone(secondsFromGMT: 0)
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
         return formatter
     }()
